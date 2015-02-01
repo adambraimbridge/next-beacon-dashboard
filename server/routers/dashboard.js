8e2567733de1e952470399d7fd031affaede1075
Matt Chadburn <matt.chadburn@ft.com>
2015-02-01 15:36:16 +0000

    Add an export link to the dashboard

--- server/routers/dashboard.js
+++ server/routers/dashboard.js
@@ -3,9 +3,11 @@ module.exports.graph = function(req, res) {
     var opts = {
         graph: true,
         title: req.query.title || '',   // XSS me
+        apiLink: req._parsedUrl.search,
         explain: req.keen_explain.join(', ')
     }
     
+
     res.render('layout.handlebars', opts);
 }
 
