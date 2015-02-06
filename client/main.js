03fed8041d0018d879c1afca04164f85a88ffe53
Matt Chadburn <matt.chadburn@ft.com>
2015-02-06 09:43:19 +0000

    Draw an 7-day  addiction graph

--- client/main.js
+++ client/main.js
@@ -5,6 +5,9 @@ switch (location.pathname) {
     case '/graph':
         require('./features/graph').init();
         break;
+    case '/addiction':
+        require('./features/addiction').init();
+        break;
     default:
         console.info('This route seems to have no corresponding javascript');
 }
