47a4657bc5328252eaaf78fba9f635a22103e514
Matt Chadburn <matt.chadburn@ft.com>
2015-02-10 14:23:02 +0000

    Nice interface for RedShift dumps

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,8 +2,9 @@
 module.exports.dashboard    = require('./dashboard');
 module.exports.data         = require('./data');
 
-var util            = require('util');
-var keenIO          = require('keen.io');
+var util        = require('util');
+var keenIO      = require('keen.io');
+var flat        = require('flat');
 
 var keen = keenIO.configure({
     projectId: process.env['KEEN_PROJECT_ID'],
@@ -12,18 +13,24 @@ var keen = keenIO.configure({
 
 // The latest 100 events we've logged
 module.exports.eventStream = function(req, res) {
+
     var latest = new keenIO.Query('extraction', {
         timeframe: req.query.timeframe || 'this_2_days',
         event_collection: req.query.event_collection || 'dwell',
         latest: req.query.limit || 100
-    });
+        });
 
     keen.run(latest, function(err, response) {
         if (err) {
             res.json(err);
             return;
         }
-        res.json(response);
+    
+        var flattened = response.result.map(function (event) {
+            return flat(event);
+        });
+
+        res.json(flattened);
     });
 }
 
