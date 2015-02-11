7b18f401045fa51a5e504337a5d7b500567e12dd
Matt Brennan <mattyb1000@gmail.com>
2015-02-11 15:59:21 +0000

    declarative filters (template)

--- server/filters.js
+++ server/filters.js
@@ -1,30 +1,49 @@
 module.exports = {
 	isStaff: {
 		property_name: "user.isStaff",
-		operator: "eq"
+		operator: "eq",
+		title: 'Staff',
+		values: {
+			false: 'Included',
+			true: 'Excluded'
+		}
 	},
 	uuid: {
 		"property_name": "page.capi.id",
-		"operator": "eq"
+		"operator": "eq",
+		hidden: true
 	},
 	pageType: {
 		"property_name": "page.location.type",
-		"operator": "eq"
+		"operator": "eq",
+		title: 'Page type',
+		values: {
+			frontpage: 'Frontpage',
+			article: 'Article',
+			stream: 'Stream',
+			search: 'Search',
+			my: 'My',
+			group: 'Group'
+		}
 	},
 	erights: {
 		"property_name": "user.erights",
-		"operator": "eq"
+		"operator": "eq",
+		hidden: true
 	},
 	flags: {
 		"property_name": "user.flags",
 		"operator": "eq",
+		hidden: true
 	},
 	domPath: {
 		"property_name": "meta.domPath",
 		"operator": "eq",
+		hidden: true
 	},
 	domPathContains: {
 		"property_name": "meta.domPath",
 		"operator": "contains",
+		hidden: true
 	}
 };
--- server/routers/dashboard.js
+++ server/routers/dashboard.js
@@ -1,10 +1,12 @@
 var graphs = require('../graphs.js');
+var filters = require('../filters.js');
 
 module.exports.graph = function(req, res) {
     
     var opts = {
         graph: true,
         graphs: graphs,
+        filters: filters,
         title: req.query.title || '',   // XSS me
         apiLink: req._parsedUrl.search,
         explain: req.keen_explain.join(', ')
@@ -18,6 +20,7 @@ module.exports.addiction = function(req, res) {
     var opts = {
         graph: true,
         graphs: graphs,
+        filters: filters,
         title: req.query.title || '',   // XSS me
         apiLink: req._parsedUrl.search,
         explain: req.keen_explain.join(', ')
