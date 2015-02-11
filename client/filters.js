0286e908372681b86b07d3f40f3f8f89292a883d
Matt Brennan <mattyb1000@gmail.com>
2015-02-11 14:50:38 +0000

    refactor filters

--- client/filters.js
+++ client/filters.js
@@ -1,80 +1,21 @@
-
-var QueryString = require('query-string');
+var qs = require('query-string');
 
 // Attach events to the Bootstrap filter dropdown menus 
 module.exports = function () {
+    var query = qs.parse(location.search);
 
-    $('[data-filter-pagetype].form-control').on('change', function (e) {
+    $('[data-filter].form-control').on('change', function (e) {
         var type = this.options[this.selectedIndex].value;
+        var key = $(this).data('filter');
         if (type) {
-            var qs = QueryString.parse(location.search)
-            qs.pageType = type;
+            query[key] = type;
             if (type === '__clear__') {
-                delete qs.pageType;
-            };
-            location.search = QueryString.stringify(qs);
+                delete query[key];
+            }
+            location.search = qs.stringify(query);
         }
-    })
-
-    $('[data-filter-staff].form-control').on('change', function (e) {
-        var type = this.options[this.selectedIndex].value;
-        if (type) {
-            var qs = QueryString.parse(location.search)
-            qs.isStaff = type;
-            if (type === '__clear__') {
-                delete qs.isStaff;
-            };
-            location.search = QueryString.stringify(qs);
-        }
-    })
-
-    $('[data-filter-timeframe].form-control').on('change', function (e) {
-        var type = this.options[this.selectedIndex].value;
-        if (type) {
-            var qs = QueryString.parse(location.search)
-            qs.timeframe = type;
-            if (type === '__clear__') {
-                delete qs.timeframe;
-            };
-            location.search = QueryString.stringify(qs);
-        }
-    })
-    
-    $('[data-filter-interval].form-control').on('change', function (e) {
-        var type = this.options[this.selectedIndex].value;
-        if (type) {
-            var qs = QueryString.parse(location.search)
-            qs.interval = type;
-            if (type === '__clear__') {
-                delete qs.interval;
-            };
-            location.search = QueryString.stringify(qs);
-        }
-    })
-
-    var qs = QueryString.parse(location.search);
-
-    if (qs.isStaff) {
-        $('[data-filter-staff].form-control').val(qs.isStaff);
-    } else {
-        $('[data-filter-staff].form-control').val('__clear__');
-    }
-
-    if (qs.pageType) {
-        $('[data-filter-pagetype].form-control').val(qs.pageType);
-    } else {
-        $('[data-filter-pagetype].form-control').val('__clear__');
-    }
-    
-    if (qs.timeframe) {
-        $('[data-filter-timeframe].form-control').val(qs.timeframe);
-    } else {
-        $('[data-filter-timeframe].form-control').val('__clear__');
-    }
-    
-    if (qs.interval) {
-        $('[data-filter-interval].form-control').val(qs.interval);
-    } else {
-        $('[data-filter-interval].form-control').val('__clear__');
-    }
+    }).each(function() {
+        var key = $(this).data('filter');
+        $(this).val(query[key] || '__clear__');
+    });
 };
