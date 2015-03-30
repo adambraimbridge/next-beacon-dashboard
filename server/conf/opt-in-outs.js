1236a7ffc582f271b94affb6161fd63e89ab6e58
Keith Rogers <git@keithrogers.eu>
2015-03-30 16:35:26 +0100

    Add another page for today's in/outs; ignore staff

--- server/conf/opt-in-outs.js
+++ server/conf/opt-in-outs.js
@@ -1,15 +1,28 @@
 module.exports = [{
-	"Opt in vs out": {
+	"Opt in vs out (today)": {
 		pathname: "opt-in-out",
 		query: {
 			"event_collection": "optin",
 			"metric": "count_unique",
 			"target_property": "user.erights",
 			"group_by": "meta.type",
-			"title": "Opt in vs out",
+			"title": "Opt in vs out (today, non-staff)",
+			"timeframe": "today",
+			"interval": "monthly",
+			"isUnknown": "user.isStaff"
+		}
+	},
+	"Opt in vs out (past week)": {
+		pathname: "opt-in-out",
+		query: {
+			"event_collection": "optin",
+			"metric": "count_unique",
+			"target_property": "user.erights",
+			"group_by": "meta.type",
+			"title": "Opt in vs out (past week, non-staff)",
 			"timeframe": "this_7_days",
 			"interval": "monthly",
-			"exists": "meta.type"
+			"isUnknown": "user.isStaff"
 		}
 	},
 	"Opt outs by reason": {
@@ -19,7 +32,7 @@ module.exports = [{
 			"metric": "count_unique",
 			"target_property": "user.erights",
 			"group_by": "meta.reason",
-			"title": "Opt outs by reason (over the past week)",
+			"title": "Opt outs by reason (past week)",
 			"timeframe": "this_7_days",
 			"interval": "daily",
 			"exists": "meta.reason",
