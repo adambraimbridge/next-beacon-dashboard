3adcfb4703090785102baf0cf58e41812859b0c2
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-04-21 17:00:48 +0100

    linting

--- server/routers/flow.js
+++ server/routers/flow.js
@@ -1,17 +1,19 @@
+'use strict';
+
 var conf = require('../conf');
 
 module.exports = function(req, res) {
 
 	var opts = {
-        graph: true,
-        graphs: conf.graphs,
-        ctas: conf.ctas,
-        filters: conf.filters,
-        flow: conf.flow,
-        ab: conf.ab,
-        title: req.query.title || '',   // XSS me
-        apiLink: req._parsedUrl.search
-    };
+		graph: true,
+		graphs: conf.graphs,
+		ctas: conf.ctas,
+		filters: conf.filters,
+		flow: conf.flow,
+		ab: conf.ab,
+		title: req.query.title || '',   // XSS me
+		apiLink: req._parsedUrl.search
+	};
 
-    res.render('main.handlebars', opts);
+	res.render('main.handlebars', opts);
 };
