d2cb43717aeb72b4530b22c0e9200ed73b8c5795
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2015-05-06 14:35:25 +0100

    Added the old version of auth.js, so it can be reimplimented for API calls.

---
+++ server/middleware/auth-api.js
@@ -0,0 +1,16 @@
+'use strict';
+var auth = require('http-auth');
+var credentials = process.env.BASIC_AUTH;
+
+if (!credentials) throw new Error("ft-next-beacon-dashboard BASIC_AUTH env *must* be set");
+
+var user = credentials.split(':')[0];
+var pass = credentials.split(':')[1];
+
+var basic = auth.basic({
+	realm: "FT"
+}, function (username, password, callback) {
+	callback(username === user && password === pass);
+});
+
+module.exports = auth.connect(basic);
