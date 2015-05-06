1c6a8465bbca3858611f14866b8f87f0ddf99928
Matt Chadburn <matt@commuterjoy.co.uk>
2015-05-06 10:31:02 +0100

    Debug HTTPS

--- server/app.js
+++ server/app.js
@@ -33,14 +33,10 @@ app.get('/', function (req, res) {
 	res.render('index.handlebars', { hideMenu: true, graphs: conf.graphs, ctas: conf.ctas, opts: conf.opts });
 });
 
-// Force HTTPS in production
-app.get('*', function(req, res, next) {
-	if (process.env.NODE_ENV === 'production' && req.protocol !== 'https') {
-		res.redirect('https://' + req.headers.host + req.url);
-	} else {
-		next();
-	}
-});
+console.log('FIXME protocol', req.protocol);
+console.log('FIXME headers', req.headers);
+console.log('FIXME host', req.headers.host);
+console.log('FIXME url', req.url);
 
 // Authenticate all routes beneath here
 app.use(cookieParser());
