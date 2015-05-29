05ce9fdaee70bc738b10accf7375031cf6f55429
Matt Chadburn <matt.chadburn@ft.com>
2015-05-29 17:00:52 +0100

    Daily uniques

--- client/beacon.js
+++ client/beacon.js
@@ -1,3 +1,28 @@
 "use strict";
 
 console.log('ok');
+
+switch (location.pathname) {
+	
+	case '/graph/uniques':
+		var client = new Keen({
+			projectId: keen_project,
+			readKey: keen_read_key
+		});
+		Keen.ready(function(){
+
+			var query = new Keen.Query("count_unique", {
+				eventCollection: "dwell",
+				target_property: "user.erights",
+				timeframe: "today"
+			});
+
+			client.draw(query, document.getElementById("my_chart"), { 
+			    title: "Unique users today"
+			});
+
+		}); 
+	default:
+		console.log('unknown graph');
+}
+
