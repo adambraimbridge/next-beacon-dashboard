0bb36ac3151084b986d30d3c4a3cfb8b5c2e698f
Matt Chadburn <matt.chadburn@ft.com>
2015-05-31 12:21:16 +0100

    Addiction

--- client/beacon.js
+++ client/beacon.js
@@ -1,5 +1,7 @@
 "use strict";
-	
+
+var _ = require('lodash');
+
 var client = new Keen({
 	projectId: keen_project,
 	readKey: keen_read_key
@@ -11,10 +13,13 @@ var container = document.getElementById("graph__container");
 var render = function (keen, opts) {
 	var el = document.createElement('div');
 	container.appendChild(el);
+	
 	if (keen.render) {
+		
 		client.run(keen.query, function (err, results) {
-			keen.render(el, results);
+			keen.render(el, results, opts);
 		});
+
 	} else {
 		client.draw(keen.query, el, opts);
 	}
@@ -24,18 +29,57 @@ Keen.ready(function(){
 
 	switch (location.pathname) {
 		
+		case '/graph/addiction':
+
+			render(require('./queries/addiction').thisWeek, { title: 'This week' });
+			render(require('./queries/addiction').lastWeek, { title: 'Last week' });
+			render(require('./queries/addiction').thisTimeLastMonth, { title: 'This time last month' });
+			break;
+		
+		case '/graph/addiction/weekly':
+
+			render(require('./queries/addiction').weekly, { title: 'Weekly addiction' });
+			render(require('./queries/addiction').weeklyThisTimeLastMonth, { title: 'Weekly - this time last month' });
+
+			break;
+
 		case '/graph/uniques/by-page':
 
 			render(require('./queries/uniques/by-page'), {
 				chartType: "areachart",
 				titlePosition: 'none',
-				legend: { position: 'bottom' },
 				isStacked: 'percent',
-				height: '400',
 				colors: ['rgb(115, 192, 58)', 'rgb(150, 85, 126)', 'rgb(101, 185, 172)', 'rgb(70, 130, 180)', 'rgb(203, 81, 58)', 'rgb(120, 95, 67)'], 
 				lineWidth: 0,
 				theme: 'maximized',
 				areaOpacity: 0.8
+			});
+
+			break;
+		
+		case '/graph/uniques/by-geo':
+
+			render(require('./queries/uniques/by-geo').continent, {
+				chartType: "areachart",
+				titlePosition: 'none',
+				isStacked: 'percent',
+				colors: ['rgb(115, 192, 58)', 'rgb(150, 85, 126)', 'rgb(101, 185, 172)', 'rgb(70, 130, 180)', 'rgb(203, 81, 58)', 'rgb(120, 95, 67)'], 
+				lineWidth: 0,
+				theme: 'maximized',
+				areaOpacity: 0.8
+			});
+			
+			render(require('./queries/uniques/by-geo').country, {
+				chartType: "table"
+			});
+
+			break;
+		
+		case '/graph/uniques/by-device':
+
+			render(require('./queries/uniques/by-device'), {
+				chartType: "columnchart",
+				height: '400'
 			});
 
 			break;
@@ -67,7 +111,7 @@ Keen.ready(function(){
 				colors: ['#77C9BC']
 			});
 			
-			render(require('./queries/uniques/two_week_average'), { });
+			render(require('./queries/uniques/two_week_average'));
 
 			break;
 	
---
+++ client/queries/uniques/by-geo.js
@@ -0,0 +1,26 @@
+
+var querystring = require('querystring');
+var qs = querystring.parse(location.search.slice(1));
+
+module.exports.continent = {
+
+	query: new Keen.Query("count_unique", {
+		eventCollection: "dwell",
+		timeframe: qs.timeframe || "this_14_days",
+		targetProperty: "user.uuid",
+		group_by: ["user.geo.continent"],
+		interval: "daily"
+	})
+
+};
+
+module.exports.country = {
+
+	query: new Keen.Query("count_unique", {
+		eventCollection: "dwell",
+		timeframe: qs.timeframe || "this_14_days",
+		targetProperty: "user.uuid",
+		group_by: ["user.geo.country_name"]
+	})
+
+};
