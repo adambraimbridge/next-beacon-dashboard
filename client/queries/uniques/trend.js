b6dc1765bce48603b31905b87d7aefdc796346e7
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2015-06-16 15:07:07 +0100

    (uniques) Updated trend (aka uniques over time)

--- client/queries/uniques/trend.js
+++ client/queries/uniques/trend.js
@@ -2,14 +2,77 @@
 
 'use strict';
 
-var querystring = require('querystring');
-var qs = querystring.parse(location.search.slice(1));
+var queryString = require('query-string');
+var queryParameters = queryString.parse(location.search);
 
-console.log(qs);
-
-module.exports.query = new Keen.Query("count_unique", {
+var query = new Keen.Query("count_unique", {
 	eventCollection: "dwell",
-	timeframe: qs.timeframe || "this_14_days",
+	timeframe: queryParameters.timeframe || "this_14_days",
+	interval: queryParameters.interval || "daily",
 	targetProperty: "user.uuid",
-	interval: "daily"
+	timezone: "UTC",
+	filters:[{
+		property_name:"user.isStaff",
+		operator:"eq",
+		property_value:false
+	}],
+	maxAge: 3600
 });
+
+var render = function (el, results, opts, client) {
+
+	var linechart = new Keen.Dataviz()
+		.el(document.getElementById("linechart"))
+		.chartType("linechart")
+		.chartOptions({
+			curveType:'function',
+			hAxis: {
+				format: 'E d'
+			},
+			chartArea: {
+				left: '10%',
+				width: '75%'
+			}
+		})
+		.title('Approximate flow over time')
+		.height(450)
+		.prepare();
+
+	var columnchart = new Keen.Dataviz()
+		.el(document.getElementById("columnchart"))
+		.chartType("columnchart")
+		.chartOptions({
+			hAxis: {
+				format: 'E d'
+			},
+			chartArea: {
+				left: '10%',
+				width: '75%'
+			}
+		})
+		.title('Daily totals in real numbers')
+		.height(450)
+		.prepare();
+
+	client.run(query, function(error, response){
+		if (error) {
+			linechart.error(error.message);
+		}
+		else {
+			linechart
+				.parseRequest(this)
+				.sortGroups("desc")
+				.render();
+
+			columnchart
+				.parseRequest(this)
+				.sortGroups("desc")
+				.render();
+		}
+	});
+};
+
+module.exports = {
+	query:query,
+	render:render
+};
