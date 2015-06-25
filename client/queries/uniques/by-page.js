aa48ebe1e30898c3592eee91128bf7ff3c5fb80e
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2015-06-25 18:50:51 +0100

    Added table of URLs with unknown page type

--- client/queries/uniques/by-page.js
+++ client/queries/uniques/by-page.js
@@ -5,19 +5,31 @@
 var queryString = require('query-string');
 var queryParameters = queryString.parse(location.search);
 
-var query = new Keen.Query("count_unique", {
-	eventCollection: "dwell",
-	timeframe: queryParameters.timeframe || "this_14_days",
-	interval: queryParameters.interval || "daily",
-	groupBy: "page.location.type",
-	targetProperty: "user.uuid",
-	timezone: "UTC",
-	filters:[{
-		property_name:"user.isStaff",
-		operator:"eq",
-		property_value:false
-	}],
-	maxAge: 3600
+// This is a base query object, for spawning queries.
+var keenQuery = function(options) {
+	var parameters = {
+		groupBy: options.groupBy,
+		eventCollection: "dwell",
+		timeframe: queryParameters.timeframe || "this_14_days",
+		targetProperty: "user.uuid",
+		timezone: "UTC",
+		filters:[{
+			property_name:"user.isStaff",
+			operator:"eq",
+			property_value:false
+		}],
+		maxAge: 3600
+	};
+
+	// Don't pass any interval parameter if it's explicitly set to false
+	if (options.interval !== false) {
+		parameters['interval'] = options.interval || queryParameters.interval || "daily";
+	}
+	return new Keen.Query("count_unique", parameters);
+};
+
+var query = keenQuery({
+	groupBy:'page.location.type'
 });
 
 var render = function (el, results, opts, client) {
@@ -93,6 +105,44 @@ var render = function (el, results, opts, client) {
 			barchart_stacked
 				.parseRequest(this)
 				.sortGroups("desc")
+				.render();
+		}
+	});
+
+	var table = new Keen.Dataviz()
+		.el(document.getElementById("table"))
+		.chartType("table")
+		.chartOptions({
+			width:"100%",
+			height:"500",
+			sortAscending:false,
+			sortColumn:1
+		})
+		.prepare();
+
+	var unknownPageTypeQuery = keenQuery({
+		groupBy:'page.location.href',
+		interval: false,
+		filters: [{
+			property_name:"page.location.type",
+			operator:"eq",
+			property_value:"unknown"
+		}]
+	});
+
+	client.run(unknownPageTypeQuery, function(error, response){
+		if (error) {
+			table.error(error.message);
+		}
+		else {
+
+			// COMPLEX:Strip the timeframe from (cached?) query results because,
+			// if it's provided, it breaks the table format
+			if (response.result.length === 1 && response.result[0].timeframe) {
+				response.result = response.result[0].value;
+			}
+			table
+				.parseRequest(this)
 				.render();
 		}
 	});
