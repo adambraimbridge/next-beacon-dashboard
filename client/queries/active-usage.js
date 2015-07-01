a5deed2ad8736b8f62702c430f50f4c8e45ce9a6
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2015-07-01 14:24:36 +0100

    Added device-specific active user funnels

--- client/queries/active-usage.js
+++ client/queries/active-usage.js
@@ -36,7 +36,7 @@ var daysFromNow = function (offset) {
 var step = function(options) {
 	return {
 		eventCollection:options.eventCollection || "dwell",
-		actor_property:"user.erights",
+		actor_property:"user.uuid",
 		timeframe:options.timeframe,
 		filters:[{
 			property_name:"user.isStaff",
@@ -46,14 +46,15 @@ var step = function(options) {
 	};
 };
 
-var activeUserStepsForFeature = function (cta) {
+var activeUserStepsForFeature = function (options) {
 	return [
 		// Users who visited next.ft in a one-week period, two weeks whence
 		step({
 			timeframe: {
 				start:daysFromNow(-14), //two weeks whence
 				end:daysFromNow(-7) //one week whence
-			}
+			},
+			filters: options.filters
 		}),
 		// Users who visited next.ft in the last 7 days
 		step({
@@ -72,28 +73,132 @@ var activeUserStepsForFeature = function (cta) {
 			filters: [{
 				property_name:"meta.domPath",
 				operator:"contains",
-				property_value:cta
+				property_value:options.cta
 			}]
 		})
 	];
 };
 
-var renderDashboard = function (el, results, opts) {
+var prepareQuery = (function () {
+	var queryAll = new Keen.Query("funnel", {
+		steps:activeUserStepsForFeature({
+			cta: features[queryParameters.feature]
+		}),
+		maxAge: 10800
+	});
 
-	var percentage = (100 / results.result[1] * results.result[2]).toFixed(2);
+	var queryLargeDevices = new Keen.Query("funnel", {
+		steps:activeUserStepsForFeature({
+			cta: features[queryParameters.feature],
+			filters: [{
+				property_name:"user.layout",
+				operator:"in",
+				property_value:["XL","L"]
+			}]
+		}),
+		maxAge: 10800
+	});
+
+	var queryMediumDevices = new Keen.Query("funnel", {
+		steps:activeUserStepsForFeature({
+			cta: features[queryParameters.feature],
+			filters: [{
+				property_name:"user.layout",
+				operator:"in",
+				property_value:["M"]
+			}]
+		}),
+		maxAge: 10800
+	});
+
+	var querySmallDevices = new Keen.Query("funnel", {
+		steps:activeUserStepsForFeature({
+			cta: features[queryParameters.feature],
+			filters: [{
+				property_name:"user.layout",
+				operator:"in",
+				property_value:["XS","S","default"]
+			}]
+		}),
+		maxAge: 10800
+	});
+
+	return [
+		queryAll,
+		queryLargeDevices,
+		queryMediumDevices,
+		querySmallDevices
+	];
+}());
+
+var metric_all = new Keen.Dataviz()
+	.chartOptions({
+		suffix: '%'
+	})
+	.title("Active usage (all devices)")
+	.el(document.getElementById("metric_all"))
+	.chartType("metric")
+	.prepare();
+
+var metric_large = new Keen.Dataviz()
+	.chartOptions({
+		suffix: '%'
+	})
+	.title("Large (XL and L)")
+	.el(document.getElementById("metric_large"))
+	.chartType("metric")
+	.prepare();
+
+var metric_medium = new Keen.Dataviz()
+	.chartOptions({
+		suffix: '%'
+	})
+	.title("Medium (M)")
+	.el(document.getElementById("metric_medium"))
+	.chartType("metric")
+	.prepare();
+
+var metric_small = new Keen.Dataviz()
+	.chartOptions({
+		suffix: '%'
+	})
+	.title("Small (S, XS and default)")
+	.el(document.getElementById("metric_small"))
+	.chartType("metric")
+	.prepare();
+
+var renderDashboard = function (el, results, opts) {
+	var resultsAll = results[0];
+	var resultsLarge = results[1];
+	var resultsMedium = results[2];
+	var resultsSmall = results[3];
+
+	var percentageAll = parseFloat((100 / resultsAll.result[1] * resultsAll.result[2]).toFixed(2));
+	var percentageLarge = parseFloat((100 / resultsLarge.result[1] * resultsLarge.result[2]).toFixed(2));
+	var percentageMedium = parseFloat((100 / resultsMedium.result[1] * resultsMedium.result[2]).toFixed(2));
+	var percentageSmall = parseFloat((100 / resultsSmall.result[1] * resultsSmall.result[2]).toFixed(2));
+
+	// See: https://github.com/keen/keen-js/blob/master/docs/visualization.md#pass-in-your-own-data-to-charts
+	metric_all
+		.parseRawData({ result:percentageAll })
+		.render();
+
+	metric_large
+		.parseRawData({ result:percentageLarge })
+		.render();
+
+	metric_medium
+		.parseRawData({ result:percentageMedium })
+		.render();
+
+	metric_small
+		.parseRawData({ result:percentageSmall })
+		.render();
 
 	$('<h1>').text('Feature: ' + queryParameters.feature).appendTo(el);
 
 	$('<div>').attr('id', 'metric')
 		.appendTo(el);
-
-	// See: https://github.com/keen/keen-js/blob/master/docs/visualization.md#pass-in-your-own-data-to-charts
-	new Keen.Dataviz()
-		.el(document.getElementById('metric'))
-		.parseRawData({ result:parseFloat(percentage) })
-		.chartType("metric")
-		.title("% Active usage")
-		.render();
 
 	$('<h3>').text('How this is calculated:').appendTo(el);
 
@@ -101,22 +206,22 @@ var renderDashboard = function (el, results, opts) {
 
 	$('<tr>')
 		.append($('<td>').text('How many users visited next.ft in a one-week period, two weeks ago?'))
-		.append($('<td>').html('<b>' + results.result[0] + ' users</b>'))
+		.append($('<td>').html('<b>' + resultsAll.result[0] + ' users</b>'))
 		.appendTo(table);
 
 	$('<tr>')
 		.append($('<td>').text('Of those users, how many visited next.ft in the last seven days?'))
-		.append($('<td>').html(results.result[1] + ' <i>active</i> users'))
+		.append($('<td>').html(resultsAll.result[1] + ' <i>active</i> users'))
 		.appendTo(table);
 
 	$('<tr>')
 		.append($('<td>').text('How many of those active users used ' + queryParameters.feature + '?*'))
-		.append($('<td>').html(results.result[2] + ' <i>active, feature-clicking</i> users'))
+		.append($('<td>').html(resultsAll.result[2] + ' <i>active, feature-clicking</i> users'))
 		.appendTo(table);
 
 	$('<tr>')
 		.append($('<td>').text("What's that as a percentage?"))
-		.append($('<td>').text(percentage + '%'))
+		.append($('<td>').text(percentageAll + '%'))
 		.appendTo(table);
 
 	table.appendTo(el);
@@ -127,9 +232,6 @@ var renderDashboard = function (el, results, opts) {
 };
 
 module.exports = {
-	query: new Keen.Query("funnel", {
-		steps:activeUserStepsForFeature(features[queryParameters.feature]),
-		maxAge: 10800
-	}),
+	query:prepareQuery,
 	render:renderDashboard
 };
