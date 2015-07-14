ab9264a7e1c2d2dbb91fbd960fc975a40ba9fd97
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2015-07-14 14:40:53 +0100

    Uniques: Added isStaff filter; added 14 day average (incl. weekends); cosmetic updates

--- client/queries/uniques/today.js
+++ client/queries/uniques/today.js
@@ -5,6 +5,14 @@
 module.exports.query = new Keen.Query("count_unique", {
 	eventCollection: "dwell",
 	target_property: "user.uuid",
-	timeframe: "today",
+	timeframe: "this_1_days",
+	timezone: "UTC",
+	filters:[{
+		property_name:"user.isStaff",
+		operator:"eq",
+		property_value:false
+	}],
 	maxAge: 10800
 });
+
+
