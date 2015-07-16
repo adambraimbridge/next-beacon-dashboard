6aaa09bb8b70cee17987f100c860cd10a06fb311
Paul I Wilson <paul.i.wilson@ft.com>
2015-07-16 15:46:05 +0100

    adding bounce rate and scroll depth

--- client/queries/anons/activity.js
+++ client/queries/anons/activity.js
@@ -9,6 +9,29 @@ var client = new Keen({
 	readKey: keen_read_key
 });
 
+var bounceRateChart = new Keen.Dataviz()
+	.chartOptions({
+		suffix: '%'
+	})
+	.title("Percentage of anonymous users viewing only one page")
+	.el(document.getElementById("bounceRate"))
+	.chartType("metric")
+	.prepare();
+
+var scrollDepthChart =new Keen.Dataviz()
+	.chartOptions({
+		legend: {position:'none'},
+		vAxis:{
+			baselineColor: '#fff',
+			textPosition: 'none'
+		}
+	})
+	.title("Amount of the article body scrolled by anon users")
+	.el(document.getElementById("scrollDepth"))
+	.chartType("columnchart")
+	.prepare();
+
+
 var pagesVisited = new Keen.Query("count_unique", {
 	eventCollection: "dwell",
 	filters: [
@@ -17,6 +40,27 @@ var pagesVisited = new Keen.Query("count_unique", {
 	],
 	groupBy: "page.location.pathname",
 	targetProperty: "user.device_id",
+	timeframe: queryParameters.timeframe || "this_14_days",
+	timezone: "UTC"
+});
+
+var bounceRateQuery =  new Keen.Query("count", {
+	eventCollection: "dwell",
+	filters: [
+		{"operator":"exists","property_name":"user.uuid","property_value":false},
+		{"operator":"exists","property_name":"meta.sessionServiceDown","property_value":false}
+	],
+	groupBy: "user.device_id",
+	targetProperty: "user.device_id",
+	timeframe: queryParameters.timeframe || "this_14_days",
+	timezone: "UTC"
+});
+
+var scrollDepthQuery = new Keen.Query("count", {
+	eventCollection: "scrolldepth",
+	filters: [{"operator":"exists","property_name":"user.uuid","property_value":false},{"operator":"exists","property_name":"meta.sessionServiceDown","property_value":false}],
+	groupBy: "meta.percentageViewed",
+	targetProperty: "meta.percentageViewed",
 	timeframe: queryParameters.timeframe || "this_14_days",
 	timezone: "UTC"
 });
@@ -32,11 +76,32 @@ client.run(pagesVisited, function(err, response){
 			var percentage = Math.round( (item.result / total) * 100) + '%';
 			ul.insertAdjacentHTML(
 				'beforeend',
-				`<li>
-					<a target="_blank" href="${url}" style="padding-right:3em">${url}</a>
-					<span>${item.result} (${percentage})</span>
-				</li>`);
+				`<tr>
+					<td><a target="_blank" href="${url}" style="padding-right:3em">${url}</a></td>
+				 	<td>${item.result}</td>
+				 	<td>${percentage}</td>
+				</tr>`);
 	});
 	document.querySelector('.loading-pageviews').style['display'] = 'none';
+});
 
+client.run(bounceRateQuery, function(err, response){
+	if(err){
+		console.error(err);
+		return;
+	}
+
+	var total = response.result.length;
+	var bouncers = response.result.filter(item => item.result === 1).length;
+	var bounceRate = Math.round((bouncers/total)*100);
+	bounceRateChart.parseRawData({result:bounceRate}).render();
+});
+
+client.run(scrollDepthQuery, function(err, response){
+	if(err){
+		console.error(err);
+		return;
+	}
+
+	scrollDepthChart.parseRawData({result:response.result}).render();
 });
