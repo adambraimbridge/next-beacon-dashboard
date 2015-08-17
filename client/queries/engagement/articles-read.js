dc2bae6b3661dd547b1922f661dfe9e07021bf05
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-08-17 16:45:54 +0100

    display as a graph

--- client/queries/engagement/articles-read.js
+++ client/queries/engagement/articles-read.js
@@ -1,47 +1,53 @@
-/* global Keen, $ */
+/* global Keen */
 'use strict';
 
 var queryString = require('query-string');
 var queryParameters = queryString.parse(location.search);
-var moment = require('moment');
 
 var render = (el, results, opts) => {
 	var weeks = [];
 	// in days
-	var groupSize = 7;
-	// take groups of x days
-	results.result.reverse().forEach((result, index) => {
-		if (index % groupSize === 0) {
-			weeks.unshift({
-				weekEnd: result.timeframe.end,
-				// two buckets, < 1.5 and >= 1.5
-				buckets: [
-					{
-						min: 1.5,
-						name: '>= 1.5',
-						value: 0
-					},
-					{
-						max: 1.5,
-						name: '< 1.5',
-						value: 0
+	var groupSize = 14;
+	// pull out days
+	results.result
+		.map(result => result['time.day'])
+		.filter((day, index, days) => days.indexOf(day) === index)
+		.sort()
+		.reverse()
+		.forEach((day, index) => {
+			if (index % groupSize === 0) {
+				weeks.unshift({
+					periodEnd: day,
+					// two buckets, < 1.5 and >= 1.5
+					buckets: [
+						{
+							min: 1.5,
+							name: '>= 1.5',
+							value: 0
+						},
+						{
+							max: 1.5,
+							name: '< 1.5',
+							value: 0
+						}
+					],
+					users: {}
+				});
+			}
+			results.result
+				.filter(result => day === result['time.day'])
+				.forEach(result => {
+					var count = result.result;
+					if (!count) {
+						return;
 					}
-				],
-				users: {}
-			});
-		}
-		result.value.forEach(value => {
-			var count = value.result;
-			if (!count) {
-				return;
-			}
-			var user = value['user.uuid'];
-			if (!weeks[0].users[user]) {
-				weeks[0].users[user] = [];
-			}
-			weeks[0].users[user].push(value.result);
+					var user = result['user.uuid'];
+					if (!weeks[0].users[user]) {
+						weeks[0].users[user] = [];
+					}
+					weeks[0].users[user].push(count);
+				});
 		});
-	});
 
 	weeks.forEach(week => {
 		Object.keys(week.users).forEach(userId => {
@@ -64,30 +70,35 @@ var render = (el, results, opts) => {
 		week.buckets.forEach(bucket => bucket.percentage = (100 / totalCount) * bucket.value);
 	});
 
-	var tableHead = $('<thead>');
-	var tableHeadRow = $('<tr>')
-		.append($('<th>').text(''));
-	weeks.forEach(week => {
-		tableHeadRow.append($('<th>').text(moment(week.weekEnd).format('MMMM Do YYYY')));
-	});
-	tableHead.append(tableHeadRow);
-
-	var tableBody = $('<tbody>');
-	// pull out a bucket definition
-	weeks[0].buckets.forEach(bucket => {
-		var tableBodyRow = $('<tr>')
-			.append($('<td>').text(bucket.name));
-		weeks.forEach(week => {
-			var percentage = week.buckets.find((b) => b.name === bucket.name).percentage.toFixed(2);
-			tableBodyRow.append($('<td>').text(`${percentage}%`));
-		});
-		tableBody.append(tableBodyRow);
-	});
-
-	$('<table>')
-		.append(tableHead)
-		.append(tableBody)
-		.appendTo(el);
+	// create a chart
+	var chart = new Keen.Dataviz();
+	chart
+		.el(el)
+		.height(500)
+		.chartType('areachart')
+		.stacked(true)
+		.indexBy('timeframe.end')
+		.chartOptions({
+			pointSize: 5
+		})
+		.prepare()
+		// change the data to the format it expects
+		.data({
+			result: weeks.map(week => {
+				return {
+					value: week.buckets.map(bucket => {
+						return {
+							result: bucket.percentage.toFixed(2),
+							label: bucket.name
+						};
+					}),
+					timeframe: {
+						end: week.periodEnd
+					}
+				};
+			})
+	 	})
+	 	.render();
 };
 
 module.exports = {
@@ -105,9 +116,9 @@ module.exports = {
 				property_value: 'article'
 			}
 		],
-		groupBy: 'user.uuid',
-		timeframe: queryParameters.timeframe || 'previous_14_day',
-		interval: 'daily',
+		groupBy: ['user.uuid', 'time.day'],
+		// 10 weeks
+		timeframe: queryParameters.timeframe || 'previous_70_day',
 		timezone: 'UTC'
 	}),
 	render: render
