0e197da660c1c93c876608d9d718600bdcede09e
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-10-22 14:36:18 +0100

    first stab at dashboard

--- client/queries/uniques/by-browser.js
+++ client/queries/uniques/by-browser.js
@@ -8,7 +8,67 @@ const device = queryParameters.device || 'all';
 const timeframeLinkEl = document.querySelector(`*[href="?timeframe=${timeframe}"]`);
 timeframeLinkEl.outerHTML = timeframeLinkEl.textContent;
 const deviceLinkEl = document.querySelector(`*[href="?device=${device}"]`);
-deviceLinkEl.outerHTML = deviceLinkEl.textContent;
+if (deviceLinkEl) {
+	deviceLinkEl.outerHTML = deviceLinkEl.textContent;
+}
+
+const loadBrowserDataHandler = (deviceFilter, totalResult, ev) => {
+	const srcElement = ev.srcElement;
+	if (document.querySelector('.table__body-row--selected') || !srcElement.parentNode.classList.contains('table__body-row')) {
+		return true;
+	}
+
+	const rowEl = srcElement.parentNode;
+	rowEl.classList.add('table__body-row--selected');
+	const browserName = rowEl.querySelector('.browser-name').textContent;
+	const browserFilters = [
+		{
+			operator: 'eq',
+			property_name: 'ua.browser.name',
+			property_value: browserName
+		}
+	];
+	if (deviceFilter) {
+		browserFilters.push(deviceFilter);
+	}
+	const browserQuery = new Keen.Query('count', {
+		eventCollection: 'dwell',
+		group_by: 'ua.browser.version',
+		filters: browserFilters,
+		timeframe,
+		timezone: 'UTC'
+	});
+	client.run(browserQuery, (err, { result }) => {
+		const browserTotal = rowEl.querySelector('.browser-count').textContent;
+		const tableContent = result
+			.sort((browserDataOne, browserDataTwo) => browserDataTwo.result - browserDataOne.result)
+			.map(browserData => {
+				const percentage = (100 / browserTotal) * browserData.result;
+				const totalPercentage = (100 / totalResult) * browserData.result;
+				return `
+					<tr class="table__body-row">
+						<td class="table__body-row__browser-version">${browserData['ua.browser.version']}</td>
+						<td>${browserData.result}</td>
+						<td>${percentage.toFixed(2)}</td>
+						<td>${totalPercentage.toFixed(2)}</td>
+					</tr>`;
+			})
+			.join('');
+
+		const tableEl = document.createElement('table');
+		tableEl.className = 'table table--show-all table--browser';
+		tableEl.innerHTML = `
+			<tr>
+				<th>${browserName} Version</th>
+				<th>Count</th>
+				<th>%</th>
+				<th>Total %</th>
+			</tr>
+			${tableContent}`;
+		document.querySelector('.browser').innerHTML = tableEl.outerHTML;
+		rowEl.classList.remove('table__body-row--selected');
+	});
+};
 
 const render = () => {
 	const deviceFilter = device === 'all' ?
@@ -38,85 +98,36 @@ const render = () => {
 	});
 	client.run(browsersQuery, (err, { result }) => {
 		const totalResult = result.reduce((currentTotal, currentBrowserData) => currentTotal + currentBrowserData.result, 0);
-		const tableEl = document.createElement('table');
-		tableEl.className = 'table table--hover table--show-all table--browsers';
 		const tableContent = result
 			.sort((browserDataOne, browserDataTwo) => browserDataTwo.result - browserDataOne.result)
 			.map(browserData => {
 				const percentage = (100 / totalResult) * browserData.result;
 				return `
 					<tr class="table__body-row">
-						<td class="table__body-row__browser-name">${browserData['ua.browser.name']}</td>
-						<td>${browserData.result}</td>
-						<td>${percentage.toFixed(2)}</td>
+						<td class="browser-name">${browserData['ua.browser.name']}</td>
+						<td class="browser-count">${browserData.result}</td>
+						<td class="browser-percentage">${percentage.toFixed(2)}</td>
 					</tr>`;
 			})
 			.join('');
+
+		const tableEl = document.createElement('table');
+		tableEl.className = 'table table--hover table--show-all table--browsers';
 		tableEl.innerHTML = `
-			<tr>
-				<th>Browser</th>
-				<th>Count</th>
-				<th>%</th>
-			</tr>
-			${tableContent}
-		`;
+			<thead>
+				<tr>
+					<th>Browser</th>
+					<th>Count</th>
+					<th>%</th>
+				</tr>
+			</thead>
+			<tbody>
+				${tableContent}
+			</tbody>`;
+		tableEl.addEventListener('click', loadBrowserDataHandler.bind(this, deviceFilter, totalResult));
+
 		document.querySelector('.browser-names')
 			.appendChild(tableEl);
-
-		document.querySelector('.table--browsers')
-			.addEventListener('click', (ev) => {
-				const srcElement = ev.srcElement;
-				if (srcElement.parentNode.classList.contains('table__body-row')) {
-					const rowEl = srcElement.parentNode;
-					const browserName = rowEl.querySelector('.table__body-row__browser-name').textContent;
-					const browserFilters = [
-						{
-							operator: 'eq',
-							property_name: 'ua.browser.name',
-							property_value: browserName
-						}
-					];
-					if (deviceFilter) {
-						browserFilters.push(deviceFilter);
-					}
-					const browserQuery = new Keen.Query('count', {
-						eventCollection: 'dwell',
-						group_by: 'ua.browser.version',
-						filters: browserFilters,
-						timeframe,
-						timezone: 'UTC'
-					});
-					client.run(browserQuery, (err, { result }) => {
-						const browserTotal = rowEl.children[1].textContent;
-						const tableEl = document.createElement('table');
-						tableEl.className = 'table table--show-all table--browser';
-						const tableContent = result
-							.sort((browserDataOne, browserDataTwo) => browserDataTwo.result - browserDataOne.result)
-							.map(browserData => {
-								const percentage = (100 / browserTotal) * browserData.result;
-								const totalPercentage = (100 / totalResult) * browserData.result;
-								return `
-									<tr class="table__body-row">
-										<td class="table__body-row__browser-version">${browserData['ua.browser.version']}</td>
-										<td>${browserData.result}</td>
-										<td>${percentage.toFixed(2)}</td>
-										<td>${totalPercentage.toFixed(2)}</td>
-									</tr>
-								`;
-							})
-							.join('');
-						tableEl.innerHTML = `
-							<tr>
-								<th>${browserName} Version</th>
-								<th>Count</th>
-								<th>%</th>
-								<th>Total %</th>
-							</tr>
-							${tableContent}`;
-						document.querySelector('.browser').innerHTML = tableEl.outerHTML;
-					});
-				}
-			})
 	});
 }
 
