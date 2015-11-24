164f364e50a52d3924b6f76ed611199d44db5618
ifeanyiisitor <ifeanyiisitor@gmail.com>
2015-11-24 16:46:07 +0000

    implemented the dashboard a bit more

--- client/queries/ab/anonymous-propensity-message.js
+++ client/queries/ab/anonymous-propensity-message.js
@@ -5,347 +5,189 @@ import client from '../../lib/wrapped-keen';
 import queryString from 'querystring';
 
 const queryParameters = queryString.parse(location.search);
-const queryTimeframe  = queryParameters.timeframe || "this_14_days";
+const queryTimeframe  = queryParameters.timeframe || "this_14_days"; // TODO: Set the proper value for this
 
 //=============================================================================================//
 // MAIN
 //=============================================================================================//
 
 export function render() {
-	generateAverageViewsPerSession();
-}
-
-//=============================================================================================//
-// ACTIONS
-//=============================================================================================//
-
-function generateAverageViewsPerSession() {
-	var slots;
-
-	prepTheViewDataSlots()
-		.then(fetchTheDataFromKeen)
-		.then(calculateAverageAndTotalFromFetchedData)
-		.then(figures => {
-			slots.average.setDataTo(figures.average);
-			slots.total.setDataTo(figures.total);
-		});
-
-	function prepTheViewDataSlots() {
-		slots = {
-			average	: dataVizSlot('#charts .js-average', 'Mean page views per session'),
-			total	: dataVizSlot('#charts .js-total', 'Total', ['#91DCD0'])
-		};
-
-		return Promise.resolve();
-	}
-
-	function fetchTheDataFromKeen() {
-		// TODO: Implement
-		return new Promise(resolve => {
-			setTimeout(()=> {
-				resolve({});
-			}, 1000)
-		});
-	}
-
-	function calculateAverageAndTotalFromFetchedData() {
-		// TODO: Implement
-		return Promise.resolve({average:15, total:30});
-	}
+	visualize(AveragePageViewsPerSession);
+	visualize(ClicksPerVisit);
+	visualize(TotalSubscriptions);
 }
 
 //=============================================================================================//
 // UTILITIES
 //=============================================================================================//
 
-function dataVizSlot(selector, title, colors) {
-	return {
-		viz: new Keen.Dataviz().el(document.querySelector(selector)).title(title).prepare(),
-		setDataTo(value) {
-			this.viz
-					.data({ result: value })
-					.render();
+function visualize(Metric) {
+	const slots = [];
+
+	prepViewSlotsFor(Metric)
+			.then(Metric.fetchData)
+			.then(Metric.prepDataForVisualisation)
+			.then(loadDataIntoViewSlots);
+
+	function prepViewSlotsFor(Metric) {
+		return Promise.resolve()
+			.then(getSlotElements)
+			.then(prepSlotForEachElement);
+
+		function getSlotElements() {
+			return document.querySelectorAll(`[data-metric=${Metric.id}] [data-viz]`);
 		}
-	};
+
+		function prepSlotForEachElement(elements) {
+			for(const element of elements) {
+				const slot = new Slot(element);
+				slot.prepare();
+				slots.push(slot);
+			}
+		}
+	}
+
+	function loadDataIntoViewSlots( data:MetricData ) {
+		for(const slot of slots) {
+			slot.render(data.for(slot))
+		}
+	}
+}
+
+//=============================================================================================//
+// OBJECTS
+//=============================================================================================//
+
+class AveragePageViewsPerSession {
+
+	static id = 'average-page-views-per-session';
+
+	static fetchData() {
+		return Promise.all([
+			fetchPageViewsPerSessionFor('variant'),
+			fetchPageViewsPerSessionFor('control')
+		])
+		.then(results => {
+			return {
+				variant: results[0],
+				control: results[1]
+			}
+		});
+
+		function fetchPageViewsPerSessionFor(group) {
+			return Promise.resolve()
+				.then(prepQuery)
+				.then(submitQueryToKeen);
+
+			function prepQuery() {
+				return new Keen.Query('count', Object.assign({
+					eventCollection: 'dwell',
+					filters: [
+						{
+							operator: 'eq',
+							property_name: 'ab.propensityMessaging',
+							property_value: group
+						}
+					],
+					groupBy: 'ingest.device.spoor_session',
+					timeframe: queryTimeframe,
+					timezone: 'UTC'
+				}))
+			}
+
+			function submitQueryToKeen(query) {
+				return new Promise(resolve => {
+					client.run([query], (err, results) => {
+						resolve(results.result);
+					});
+				})
+			}
+		}
+	}
+
+	static prepDataForVisualisation(data) {
+		return Promise.all([
+			calculateAverageOf(data['variant']),
+			calculateAverageOf(data['control'])
+		])
+		.then(returnDataAsMetricData);
+
+		function calculateAverageOf(items) {
+			return new Promise(resolve => {
+
+				resolve(totalViews() / items.length);
+
+				function totalViews() {
+					let total = 0;
+					for(const item of items) {
+						total = total + item.result;
+					}
+					return total;
+				}
+			});
+		}
+
+		function returnDataAsMetricData(averages) {
+			return new MetricData({
+				variant:averages[0],
+				control:averages[1]
+			});
+		}
+	}
 }
 
 
+class ClicksPerVisit {
 
+	static id = 'clicks-per-visit';
 
+	static fetchData() {
+		// TODO: Implement
+	}
 
+	static prepDataForVisualisation(data) {
+		// TODO: Implement
+		return new MetricData({variant:20, control:40})
+	}
+}
 
+class TotalSubscriptions {
 
+	static id = 'total-subscriptions';
 
+	static fetchData() {
+		// TODO: Implement
+	}
 
+	static prepDataForVisualisation(data) {
+		// TODO: Implement
+		return new MetricData({variant:0, control:250})
+	}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var generateAverageViews = (el, type, state, queryOpts = {}) => {
-
-	const averageContainerElem = el.querySelector('.js-average');
-	const average = new Keen.Dataviz().el(averageContainerElem).prepare();
-
-	const totalContainerElem = el.querySelector('.js-total');
-	const total = new Keen.Dataviz().el(totalContainerElem).prepare();
-
-	var pageViewsQueries = [
-		new Keen.Query('count', Object.assign({
-			eventCollection: 'dwell',
-			filters: [
-				{
-					operator: 'eq',
-					property_name: 'ab.propensityMessaging',
-					property_value: 'variant'
-				},
-				{
-					operator: 'exists',
-					property_name: 'user.uuid',
-					property_value: true
-				}
-			],
-			groupBy: 'ingest.device.spoor_session',
-			timeframe: queryTimeframe,
-			timezone: 'UTC'
-		}, queryOpts))
-	];
-
-	client.run(pageViewsQueries, (err, results) => {
-		var data = results.result;
-
-		// remove any session > 20 page views as these are outliers
-		/*
-		var clean = data.filter(function (a) {
-			return (a.result <= 20);
-		});
-		*/
-
-		var average = clean.reduce(function(prev, current) {
-			return prev + current.result;
-		}, 0) / clean.length;
-
-		var avg = 10;
-		var ttl = 20;
-
-		average
-			.data({ result: avg })
-			.title('Mean page views per session')
-			.render();
-
-		total
-			.data({ result: ttl })
-			.colors(['#91DCD0'])
-			.title('Total')
-			.render();
-	});
-};
-
-
-
-
-
-
-
-
-/*
-var generateFrequency = (timeframe, id, filters=[]) => {
-	var keenQuery = function(options) {
-		var query = options.query || 'count_unique';
-		var parameters = {
-			eventCollection: options.eventCollection || "dwell",
-			timeframe: queryTimeframe,
-			targetProperty: options.targetProperty,
-			timezone: "UTC",
-			filters:options.filters || [],
-			maxAge: 10800
-		};
-
-		if (options.groupBy) {
-			parameters['groupBy'] = options.groupBy;
+class Slot {
+	constructor(element) {
+		this.id = element.dataset.viz;
+	    this.instance = new Keen.Dataviz().el(element).title(element.dataset.vizTitle);
+		if(element.dataset.vizColors) {
+			this.instance.colors(element.dataset.vizColors.split(','))
 		}
+	}
 
-		// Don't pass any interval parameter if it's explicitly set to false
-		if (options.interval !== false) {
-			parameters['interval'] = options.interval || queryParameters.interval || "daily";
-		}
+	prepare() {
+		this.instance.prepare();
+	}
+	render(data) {
+		this.instance.data({ result: data }).render();
+	}
+}
 
-		return new Keen.Query(query, parameters);
-	};
-	var metricAverageFrequency = new Keen.Dataviz()
-		.el(document.getElementById("metric_average_frequency__" + id))
-		.chartType("metric")
-		.title(timeframe.replace(/_/g, ' ') + ' for variant ' + id)
-		.height(140)
-		.prepare();
+class MetricData {
+	constructor(data) {
+		this.data = data || {};
+	}
 
-	var queryLastVisitPerUser = keenQuery({
-		timeframe: timeframe,
-		query: 'maximum',
-		targetProperty: 'time.day',
-		groupBy: 'user.uuid',
-		interval: false,
-		filters: filters
-	});
-
-	var queryVisitsPerUser = keenQuery({
-		timeframe: timeframe,
-		targetProperty: 'time.day',
-		groupBy: 'user.uuid',
-		interval: false,
-		filters: filters
-	});
-
-
-	client.run([queryVisitsPerUser, queryLastVisitPerUser], function() {
-		var visitsPerUser = this.data[0].result;
-		var totalUniqueUsers = visitsPerUser.length;
-
-		//Work out the average days visiting the site in the timeframe
-		var averageVisitsPerUser = visitsPerUser.reduce(function(memo, user) {
-			return memo + user.result;
-		}, 0) / totalUniqueUsers;
-
-		metricAverageFrequency
-			.parseRawData({ result:parseFloat(averageVisitsPerUser) })
-			.render();
-	});
-
-
-};
-
-var generateOptin = function(id, filters) {
-	var KeenQuery = function(options) {
-		var parameters = {
-			eventCollection: 'optin',
-			timeframe: queryTimeframe,
-			targetProperty: 'user.uuid',
-			groupBy: options.groupBy || 'meta.type',
-			timezone: 'UTC',
-			filters: filters || [],
-			maxAge: 10800
-		};
-
-		// Don't pass any interval parameter if it's explicitly set to false
-		if (options.interval !== false) {
-			parameters['interval'] = options.interval || queryParameters.interval || 'daily';
-		}
-
-		return new Keen.Query(options.queryType || 'count_unique', parameters);
-	};
-	var piechart_24_hours = new Keen.Dataviz()
-		.el(document.getElementById('piechart__' + id))
-		.chartType('piechart')
-		.colors(['#6FF187','#FF6060'])
-		.title('Snapshot for variant ' + id)
-		.height(250)
-		.prepare();
-
-	var piechart24HoursQuery = new KeenQuery({
-		interval: false,
-		timeframe: 'this_24_hours'
-	});
-
-	client.run([
-		piechart24HoursQuery
-	], function(error, response){
-		if (error) {
-			throw new Error('Keen query error: ' + error.message);
-		}
-		else {
-			piechart_24_hours
-				.parseRawData({result:response.result})
-				.render();
-		}
-	});
-};
-
-var generateOptoutReason = function(id) {
-	var KeenQuery = function(options) {
-		var parameters = {
-			eventCollection: 'optin',
-			timeframe: queryTimeframe,
-			targetProperty: 'user.uuid',
-			groupBy: 'meta.reason',
-			timezone: 'UTC',
-			filters: [{
-				property_name:'meta.reason',
-				operator:'exists',
-				property_value:true
-			},
-			{
-				property_name:'meta.reason',
-				operator:'not_contains',
-				property_value:'unknown'
-			},
-			{
-				property_name:'meta.reason',
-				operator:'not_contains',
-				property_value:'other'
-			},{
-				operator: 'eq',
-				property_name: 'ab.performanceAB',
-				property_value: id
-			}],
-			maxAge: 10800
-		};
-
-		// Don't pass any interval parameter if it's explicitly set to false
-		if (options.interval !== false) {
-			parameters['interval'] = options.interval || queryParameters.interval || 'daily';
-		}
-
-		return new Keen.Query(options.queryType || 'count_unique', parameters);
-	};
-	var optOutChart = new Keen.Dataviz()
-		.el(document.getElementById("optout__" + id))
-		.chartType("metric")
-		.title('Percentage of opt outs that are too-slow for variant ' + id)
-		.height(140)
-		.prepare();
-
-	var optOutReasonQuery = new KeenQuery({
-		queryType: 'count',
-		interval: false,
-	});
-
-	client.run([
-		optOutReasonQuery
-	], function(error, response){
-		var total = response.result.reduce(function(prev, current) {
-			return prev + current.result;
-		}, 0);
-		var slowTotal = response.result.find(function(item) {
-			return item['meta.reason'] === 'too-slow';
-		}).result;
-		if (error) {
-			throw new Error('Keen query error: ' + error.message);
-		}
-		else {
-			optOutChart
-				.data({
-					result: parseFloat(((100 / total) * slowTotal).toFixed(1))
-				})
-				.render();
-		}
-	});
-};
-*/
+	for(slot) {
+		return this.data[slot.id];
+	}
+}
