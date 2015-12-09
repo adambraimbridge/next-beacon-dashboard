3ab37a9fb3f1e061fa925f933e36755e4291d526
andygout <andy.gout@ft.com>
2015-12-09 16:27:57 +0000

    Add: User type filter (subscriber/registered/anonymous)

--- client/queries/front-page/scroll-depth.js
+++ client/queries/front-page/scroll-depth.js
@@ -1,12 +1,47 @@
 /* global Keen */
 'use strict';
 
+var queryString = require('querystring');
+var queryParameters = queryString.parse(location.search.substr(1));
+
 var client = require('../../lib/wrapped-keen');
 
+const userTypes = ['subscriber', 'registered', 'anonymous'];
+
 var render = () => {
+	var currentUserType = queryParameters['user-type'] || userTypes[0];
+	var el = document.getElementById('charts');
+
+	var scrollDepthEl = document.createElement('div');
+	scrollDepthEl.classList.add('o-grid-row');
+	scrollDepthEl.innerHTML = `<h2 data-o-grid-colspan="12">Percentage of visitors that see the 1st component, 2nd component, etc</h2>`;
+	el.appendChild(scrollDepthEl);
+
+	// add user type toggle
+	var userTypeEl = document.createElement('div');
+	userTypeEl.classList.add('nav--horizontal');
+	userTypeEl.dataset.oGridColspan = '12';
+	var userItems = userTypes
+		.map(userType =>
+			userType === currentUserType ? `<li>${userType}</li>` : `<li><a href="?user-type=${userType}">${userType}</a></li>`
+		)
+		.join('');
+	userTypeEl.innerHTML = `
+		<h3>User type: </h3>
+		<ul>
+			${userItems}
+		</ul>
+	`;
+	scrollDepthEl.insertBefore(userTypeEl, scrollDepthEl.firstChild);
+	
+	var graphEl = document.createElement('div');
+	graphEl.dataset.oGridColspan = '12';
+	graphEl.classList.add('o-tabs__tabpanel');
+	scrollDepthEl.appendChild(graphEl);
+
 	const scrollDepthChart = new Keen.Dataviz()
 		.chartType('columnchart')
-		.el(document.getElementById('charts'))
+		.el(graphEl)
 		.height(450)
 		.chartOptions({
 			isStacked: true
@@ -35,6 +70,11 @@ var render = () => {
 				operator: 'exists',
 				property_name: 'meta.domPath',
 				property_value: true
+			},
+			{
+				operator: 'eq',
+				property_name: 'cohort.incumbent',
+				property_value: currentUserType
 			}
 		],
 		groupBy: ['meta.componentPos', 'meta.domPath'],
@@ -71,6 +111,13 @@ var render = () => {
 		scrollDepthChart
 			.data({ result })
 			.render();
+
+		var tabsEl = document.createElement('ul');
+		tabsEl.dataset.oComponent = 'o-tabs';
+		tabsEl.dataset.oGridColspan = '12';
+		tabsEl.className = 'o-tabs o-tabs--buttontabs';
+		tabsEl.setAttribute('role', 'tablist');
+		window.Origami['o-tabs'].init();
 	});
 };
 
