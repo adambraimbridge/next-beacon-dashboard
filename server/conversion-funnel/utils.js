d038b1c1584e9c9b25d82ce24b3e0122c21b8dac
TheoLeanse <theoleanse@gmail.com>
2016-01-26 16:16:14 +0000

    add funnel charts

--- server/conversion-funnel/utils.js
+++ server/conversion-funnel/utils.js
@@ -2,28 +2,20 @@ const filterByKeyValuePair = (data, key, value) => {
 	return data.filter(element => element[key] === value);
 };
 
-const aggregateValues = (data, key) => {
-	return data.reduce((accumulator, element) => {
-		accumulator[element[key]] = (accumulator[element[key]] || 0) + 1;
-		return accumulator;
-	}, {});
-};
-
 module.exports = (data) => {
-	const nextData = filterByKeyValuePair(data, 'channel', 'next');
-	const nextAnonData = filterByKeyValuePair(nextData, 'sub_cohort', '1');
-	const allDataTypes = Object.keys(nextAnonData[0]);
-	const unwantedDataTypes = ['visit_id', 'sub_cohort'];
+	const next = filterByKeyValuePair(data, 'channel', 'next');
+	const ft = filterByKeyValuePair(data, 'channel', 'desktop');
 
-	const formatted = allDataTypes.map(dataType => {
-		if(unwantedDataTypes.indexOf(dataType) === -1) {
-			return { [dataType]: aggregateValues(nextAnonData, dataType) };
-		}
-	});
+	const nextAnons = filterByKeyValuePair(next, 'sub_cohort', '1');
+	const ftAnons = filterByKeyValuePair(ft, 'sub_cohort', '1');
+
+	const nextAnonsMobile = filterByKeyValuePair(nextAnons, 'mobile_device', 'Mobile Phone');
+	const ftAnonsMobile = filterByKeyValuePair(ftAnons, 'mobile_device', 'Mobile Phone');
 
 	return {
-		total: nextAnonData.length,
-		mobile: filterByKeyValuePair(nextAnonData, 'mobile_device', 'Mobile Phone').length,
-		formatted: formatted
+		nextAnons: nextAnons.length,
+		ftAnons: ftAnons.length,
+		nextAnonsMobile: nextAnonsMobile.length,
+		ftAnonsMobile: ftAnonsMobile.length
 	};
 };
